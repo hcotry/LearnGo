853d337c0d62f0dd6a1e4419f6ed8612b79ee3eb
hcotry <90833622+hcotry@users.noreply.github.com>
2024-08-19 08:57:36 +0800

    Update Hello.go

--- Hello.go
+++ Hello.go
@@ -2,11 +2,9 @@ package main
 
 import "fmt"
 
-func main() { // { 不能在单独的行上
-	//{  // 错误，{ 不能在单独的行上
+func main() { 
+	
 	fmt.Printf("hello world \n")
-
-	// %d 表示整型数字，%s 表示字符串
 	var stockcode = 123
 	var enddate = "2020-12-31"
 	var url = "Code=%d&endDate=%s"
